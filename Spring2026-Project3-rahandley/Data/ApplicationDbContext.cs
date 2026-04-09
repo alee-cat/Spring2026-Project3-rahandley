042ead6643e7284f55ae94f46c63b753d6f39977
alyhandley <tomnookh8tr@gmail.com>
2026-04-08 19:53:12 -0500

    Initial commit

---
+++ Spring2026-Project3-rahandley/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spring2026_Project3_rahandley.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
